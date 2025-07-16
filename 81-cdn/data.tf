5be99ff64f70a18001e36abc277adcb81a9d68ec
rajolinaveenkumar <rajolinaveenkumar6@gmail.com>
2025-07-16 18:01:02 +0530

    jenkins

--- 81-cdn/data.tf
+++ 81-cdn/data.tf
@@ -6,8 +6,8 @@ data "aws_cloudfront_cache_policy" "cacheEnable" {
   name = "Managed-CachingOptimized"
 }
 
-data "aws_ssm_parameter" "ingress_alb_certificate_arn" {
-  name = "/${var.project_name}/${var.environment}/web_alb_listener_arn"
+data "aws_ssm_parameter" "ssl_cert_arn" {
+  name = "/${var.project_name}/${var.environment}/ssl_cert_arn"
 }
 
 data "aws_route53_zone" "zone_info" {
